1ddfc2124124495a5570e218d93d593d1dd23451
dFaery <137483979+dFaery@users.noreply.github.com>
2025-04-18 23:26:23 +0700

    Update class Buku
    Tambah bacaData, bisa menampilkan daftar buku di dgv

--- ProjectISA_uuuISA/uuuISA_Class/Buku.cs
+++ ProjectISA_uuuISA/uuuISA_Class/Buku.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using class_uuuISA;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +22,26 @@ namespace ProjectISA_uuuISA
         string pengarang;
         #endregion
 
+        #region CONSTRUCTOR
+        public Buku(int idBuku, string judul, string jenis, int jumlahHalaman, int jumlahBuku, string lokasi, string bahasa, string pengarang)
+        {
+            this.IdBuku = idBuku;
+            this.Judul = judul;
+            this.Jenis = jenis;
+            this.JumlahHalaman = jumlahHalaman;
+            this.JumlahBuku = jumlahBuku;
+            this.Lokasi = lokasi;
+            this.Bahasa = bahasa;
+            this.Pengarang = pengarang;
+        }
+
+        public Buku()
+        {
+
+        }
+
+        #endregion
+
         #region PROPERTIES
         public int IdBuku { get => idBuku; set => idBuku = value; }
         public string Judul { get => judul; set => judul = value; }
@@ -28,6 +51,33 @@ namespace ProjectISA_uuuISA
         public string Lokasi { get => lokasi; set => lokasi = value; }
         public string Bahasa { get => bahasa; set => bahasa = value; }
         public string Pengarang { get => pengarang; set => pengarang = value; }
+        #endregion
+
+        #region METHOD
+        public static List<Buku> BacaData()
+        {
+            string perintah = "SELECT * FROM buku;";
+
+            List<Buku> listBuku = new List<Buku>();
+            MySqlDataReader hasil = Koneksi.JalankanPerintahSelect(perintah);
+            Buku buku;
+
+            while(hasil.Read())
+            {
+                int idBuku = int.Parse(hasil.GetValue(0).ToString());
+                string judulBuku = hasil.GetValue(1).ToString();
+                string jenisBuku = hasil.GetValue(2).ToString();
+                int jumlahHalaman = int.Parse(hasil.GetValue(3).ToString());
+                int jumlahBuku = int.Parse(hasil.GetValue(4).ToString());
+                string lokasiBuku = hasil.GetValue(5).ToString();
+                string bahasaBuku = hasil.GetValue(6).ToString();
+                string pengarangBuku = hasil.GetString(7).ToString();
+
+                buku = new Buku(idBuku, judulBuku, jenisBuku, jumlahHalaman, jumlahBuku, lokasiBuku, bahasaBuku, pengarangBuku);
+                listBuku.Add(buku);
+            }
+            return listBuku;
+        }
         #endregion
     }
 }
