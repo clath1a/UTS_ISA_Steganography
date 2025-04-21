638f8527d22a187dc9e4d4fbc7255934bbbd8d61
dFaery <137483979+dFaery@users.noreply.github.com>
2025-04-21 22:57:34 +0700

    update fitur peminjaman buku
    pustakawan bisa acc / decline

--- ProjectISA_uuuISA/ProjectISA_uuuISA/User Control/UC_KonfirmasiPeminjamanBuku.cs
+++ ProjectISA_uuuISA/ProjectISA_uuuISA/User Control/UC_KonfirmasiPeminjamanBuku.cs
@@ -16,5 +16,68 @@ namespace ProjectISA_uuuISA.User_Control
         {
             InitializeComponent();
         }
+
+        private void UC_KonfirmasiPeminjamanBuku_Load(object sender, EventArgs e)
+        {
+            DisplayPeminjamanMasuk();
+            //Console.WriteLine("JUMLAH KOLOM: " + dataGridViewKonfirmasiPeminjamanBuku.Columns.Count);            
+            if (dataGridViewKonfirmasiPeminjamanBuku.Columns.Count == 6)
+            {
+                DataGridViewButtonColumn btnPinjam = new DataGridViewButtonColumn();
+                btnPinjam.Text = "TERIMA";
+                btnPinjam.HeaderText = "TERIMA";
+                btnPinjam.UseColumnTextForButtonValue = true;
+                btnPinjam.Name = "btnTerimaPeminjaman";
+                dataGridViewKonfirmasiPeminjamanBuku.Columns.Add(btnPinjam);
+
+                DataGridViewButtonColumn btnTolak = new DataGridViewButtonColumn();
+                btnTolak.Text = "TOLAK";
+                btnTolak.HeaderText = "TOLAK";
+                btnTolak.UseColumnTextForButtonValue = true;
+                btnTolak.Name = "btnTolakPeminjaman";
+                dataGridViewKonfirmasiPeminjamanBuku.Columns.Add(btnTolak);
+            }
+        }
+
+        private void DisplayPeminjamanMasuk()
+        {
+            List<RiwayatPeminjaman> riwayatPeminjaman = RiwayatPeminjaman.BacaData();
+            dataGridViewKonfirmasiPeminjamanBuku.DataSource = riwayatPeminjaman;
+        }
+
+        private void dataGridViewKonfirmasiPeminjamanBuku_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {                
+                int idPustakawan = FormUtama.pustakawan.IdPustakawan;
+                int idSiswa = (int)(dataGridViewKonfirmasiPeminjamanBuku.CurrentRow.Cells["idSiswa"].Value);
+                int idBuku = (int)(dataGridViewKonfirmasiPeminjamanBuku.CurrentRow.Cells["idbuku"].Value);
+                
+                if (e.ColumnIndex == dataGridViewKonfirmasiPeminjamanBuku.Columns["btnTerimaPeminjaman"].Index)
+                {                    
+                    DialogResult result = MessageBox.Show("Anda akan menerima permintaan peminjaman buku" + idBuku, "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        RiwayatPeminjaman.Update_KonfirmasiPeminjamanBuku(idPustakawan, 2, idSiswa, idBuku);
+                        MessageBox.Show("Permintaan diterima");
+                    }
+                }
+                else if(e.ColumnIndex == dataGridViewKonfirmasiPeminjamanBuku.Columns["btnTolakPeminjaman"].Index)
+                {
+                    DialogResult result = MessageBox.Show("Anda akan menolak permintaan peminjaman buku" + idBuku, "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                    if (result == DialogResult.Yes)
+                    {
+                        RiwayatPeminjaman.Update_KonfirmasiPeminjamanBuku(idPustakawan, 3, idSiswa, idBuku);
+                        MessageBox.Show("Permintaan ditolak");
+                    }                    
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
--- ProjectISA_uuuISA/uuuISA_Class/RiwayatPeminjaman.cs
+++ ProjectISA_uuuISA/uuuISA_Class/RiwayatPeminjaman.cs
@@ -1,29 +1,89 @@
-﻿using System;
+﻿using class_uuuISA;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using uuuISA_Class;
 
 namespace ProjectISA_uuuISA
 {
     public class RiwayatPeminjaman
     {
         #region DATA MEMBER
-        Siswa idSiswa;
-        Buku idBuku;
-        DateTime tanggalPeminjaman;
-        DateTime tanggalPengembalian;
-        Pustakawan idPustakawan;
+        private int idSiswa;
+        private int idBuku;
+        private DateTime tanggalPeminjaman;
+        private DateTime tanggalPengembalian;
+        private int idPustakawan;
+        private Status_Peminjaman statusPeminjaman;
+        
+
+
+        #endregion
+
+        #region CONSTUCTOR
+        public RiwayatPeminjaman(DateTime tanggalPeminjaman, int idSiswa, int idBuku, Status_Peminjaman status)
+        {
+            this.TanggalPeminjaman = tanggalPeminjaman;            
+            this.IdSiswa = idSiswa;
+            this.IdBuku = idBuku;
+            this.StatusPeminjaman = status;
+        }
         #endregion
 
         #region PROPERTIES
         public DateTime TanggalPeminjaman { get => tanggalPeminjaman; set => tanggalPeminjaman = value; }
-        public DateTime TanggalPengembalian { get => tanggalPengembalian; set => tanggalPengembalian = value; }
-        public Siswa IdSiswa { get => idSiswa; set => idSiswa = value; }
-        public Buku IdBuku { get => idBuku; set => idBuku = value; }
-        public Pustakawan IdPustakawan { get => idPustakawan; set => idPustakawan = value; }
+        public DateTime TanggalPengembalian { get => tanggalPengembalian; set => tanggalPengembalian = value; }        
+        public Status_Peminjaman StatusPeminjaman { get => statusPeminjaman; set => statusPeminjaman = value; }
+        public int IdSiswa { get => idSiswa; set => idSiswa = value; }
+        public int IdBuku { get => idBuku; set => idBuku = value; }
+        public int IdPustakawan { get => idPustakawan; set => idPustakawan = value; }
         #endregion
 
+        #region METHOD
+        public static List<RiwayatPeminjaman> BacaData()
+        {           
+            string perintah = "SELECT r.siswa_idsiswa, r.buku_idbuku, r.tanggal_peminjaman, sp.id AS status_id, sp.status " +
+                "FROM riwayat_peminjaman r  " +
+                "inner join status_peminjaman sp on sp.id = r.status_peminjaman_id " +
+                "WHERE status_peminjaman_id = 1;";
 
+            MySqlDataReader dr = Koneksi.JalankanPerintahSelect(perintah);
+
+            List<RiwayatPeminjaman> listPeminjaman = new List<RiwayatPeminjaman>();
+
+            while(dr.Read())
+            {
+                int idSiswa = int.Parse(dr.GetValue(0).ToString());
+                int idBuku = int.Parse(dr.GetValue(1).ToString());
+                DateTime tglPeminjaman = DateTime.Parse(dr.GetValue(2).ToString());                
+
+                int idStatus = int.Parse(dr.GetValue(3).ToString());
+                string status = dr.GetValue(4).ToString();
+
+                Status_Peminjaman statusPeminjaman = new Status_Peminjaman(idStatus, status);
+                RiwayatPeminjaman rp = new RiwayatPeminjaman(tglPeminjaman, idSiswa, idBuku, statusPeminjaman);
+
+                listPeminjaman.Add(rp);
+            }
+            return listPeminjaman;
+        }
+
+        public static bool Update_KonfirmasiPeminjamanBuku(int idPustakawan, int idStatus_peminjaman, int idSiswa, int idBuku)
+        {
+            string perintah = "UPDATE `riwayat_peminjaman` " +
+                "SET `pustakawan_idpustakawan` = '" + idPustakawan + "', `status_peminjaman_id` = '" + idStatus_peminjaman + "' " +
+                "WHERE (`siswa_idsiswa` = '" + idSiswa + "') and (`buku_idbuku` = '" + idBuku + "');";
+
+            int hasil = Koneksi.JalankanPerintahDML(perintah);
+            if (hasil > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
     }
 }
